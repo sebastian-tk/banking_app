cab6ae59650c337c075660b450052cc26e3670e7
Sebastian-jv <sebastian.tkaczykk@gmail.com>
2021-10-12 18:44:14 +0200

    v 21.0 modified ValidatorPersonalDataa

--- src/main/java/org/app/customer/ValidatorPersonalData.java
+++ src/main/java/org/app/customer/ValidatorPersonalData.java
@@ -8,6 +8,9 @@ package org.app.customer;
      *          example pattern "ul. Adama Mickiewicza 10/23 10-100 Warszawa"
      */
      static boolean isAddressNotCorrect(String address){
+         if(address == null || address.isEmpty()){
+             throw new IllegalArgumentException("Invalid address argument when check syntax address");
+         }
         return !address.matches("(ul\\.( [A-Z][a-z]+)+) ([0-9]\\d{0,2}(/[0-9]\\d{0,2})?) ([0-9]{2}-[0-9]{3}) ([A-Z][a-z]+)");
     }
 
@@ -18,7 +21,10 @@ package org.app.customer;
      *          pattern: firstpart.secondpart@gmail.com/outlook.com/wp.pl/onet.pl
      */
      static boolean isEmailNotCorrect(String email){
-        return !email.matches("[a-z]{1,20}\\.[a-z]{1,20}@((((gmail)|(outlook))\\.com)|(((onet)|(wp))\\.pl))");
+         if(email == null || email.isEmpty()){
+             throw new IllegalArgumentException("Invalid email argument when check syntax email");
+         }
+         return !email.matches("[a-z]{1,20}\\.[a-z]{1,20}@((((gmail)|(outlook))\\.com)|(((onet)|(wp))\\.pl))");
     }
 
     /**
@@ -28,6 +34,9 @@ package org.app.customer;
      *          separated by '-' three digits each, for example: 123-456-789
      */
      static boolean isNumberNotCorrect(String number){
+         if(number == null || number.isEmpty()){
+             throw new IllegalArgumentException("Invalid number argument when check syntax phone number");
+         }
         return !number.matches("(\\d{3}-){2}\\d{3}");
     }
 
@@ -38,6 +47,9 @@ package org.app.customer;
      *          the first of which is capital
      */
      static boolean isNameOrSurnameNotCorrect(String expression){
+         if(expression == null || expression.isEmpty()){
+             throw new IllegalArgumentException("Invalid expression argument when check syntax name or surname");
+         }
         return !expression.matches("^[A-Z][a-z]{2,}$");
     }
 }
